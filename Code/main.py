9d6c4469633ff43a79a8b2b318688e1e2c8e1250
Tabish Ali Rather <tabishrather7006@gmail.com>
2023-04-08 14:43:15 +1000

    scraper working somewhat

--- Code/main.py
+++ Code/main.py
@@ -9,4 +9,4 @@ import glassdoor_scraper as gs
 import pandas as pd
 path = "C:/Users/tabis/OneDrive - Swinburne University/Semester I - 2023/Easter break_Project/ds_salary_proj/chromedriver_win32/chromedriver.exe"
 df = gs.get_jobs("data scientist", 5, False, path, 5)
-df
+print(df)
