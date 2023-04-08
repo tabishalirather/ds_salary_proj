3f1f4d01dc0c82499c481726a7f6e17b31bc66df
Tabish Ali Rather <tabishrather7006@gmail.com>
2023-04-08 14:38:02 +1000

    improvement in webscraper

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ Created on Thu Apr  6 23:43:10 2023
 import glassdoor_scraper as gs
 import pandas as pd
 path = "C:/Users/tabis/OneDrive - Swinburne University/Semester I - 2023/Easter break_Project/ds_salary_proj/chromedriver_win32/chromedriver.exe"
-df = gs.get_jobs("data scientist", 5, False, path, )
+df = gs.get_jobs("data scientist", 5, False, path, 5)
 df
